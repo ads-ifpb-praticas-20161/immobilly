4cd9d2880d4102574f01c5420e90e39b2e1cb492
Jéderson Gonçalves <jedersongmoura@gmail.com>
2016-07-19 10:49:10 -0300

    ajuste na Classe Abastecimento

--- immobilly-shared/src/main/java/br/edu/ifpb/ads/praticas/immobilly/shared/beans/Abastecimento.java
+++ immobilly-shared/src/main/java/br/edu/ifpb/ads/praticas/immobilly/shared/beans/Abastecimento.java
@@ -37,4 +37,83 @@ public class Abastecimento implements Serializable {
     private BigDecimal valorAbastecimento;
     
     private int quantidade;
+
+    public Abastecimento() {
+    }
+
+    public Abastecimento(Veiculo veiculo, Fornecedor fornecedor, Date horario, Integer km, BigDecimal valorPorLitro, BigDecimal valorAbastecimento, int quantidade) {
+        this.veiculo = veiculo;
+        this.fornecedor = fornecedor;
+        this.horario = horario;
+        this.km = km;
+        this.valorPorLitro = valorPorLitro;
+        this.valorAbastecimento = valorAbastecimento;
+        this.quantidade = quantidade;
+    }
+
+    public AbastecimentoId getAbastecimentoId() {
+        return abastecimentoId;
+    }
+
+    public void setAbastecimentoId(AbastecimentoId abastecimentoId) {
+        this.abastecimentoId = abastecimentoId;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+    public Date getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Date horario) {
+        this.horario = horario;
+    }
+
+    public Integer getKm() {
+        return km;
+    }
+
+    public void setKm(Integer km) {
+        this.km = km;
+    }
+
+    public BigDecimal getValorPorLitro() {
+        return valorPorLitro;
+    }
+
+    public void setValorPorLitro(BigDecimal valorPorLitro) {
+        this.valorPorLitro = valorPorLitro;
+    }
+
+    public BigDecimal getValorAbastecimento() {
+        return valorAbastecimento;
+    }
+
+    public void setValorAbastecimento(BigDecimal valorAbastecimento) {
+        this.valorAbastecimento = valorAbastecimento;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+    
 }
